2fe9d04a0c40cba407c6047e75aa2ff52624e5e3
Roman Smirnov <roman@railsdog.com>
2011-11-21 23:09:31 +0400

    Changed print links in admin/orders#show template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  match "/admin/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :print_order
+  match "/admin/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :admin_print_order
 end
