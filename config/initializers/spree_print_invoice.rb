e13b15f8cba43d32daadf17ef3eef611bf0ab342
Bounmy Stephane <stephanebounmy@gmail.com>
2012-03-23 09:39:20 +0100

    use file:// only when development mode

--- config/initializers/spree_print_invoice.rb
+++ config/initializers/spree_print_invoice.rb
@@ -1,9 +1,11 @@
 PrintInvoice::Config = PrintInvoice::PrintConfiguration.new
 
-ActionController::Base.asset_host = Proc.new { |source, request|
-  if request.env["REQUEST_PATH"].include? ".pdf"
-    "file://#{Rails.root.join('app')}"
-  else
-    "#{request.protocol}#{request.host_with_port}"
-  end
-}
+if Rails.env.development?
+  ActionController::Base.asset_host = Proc.new { |source, request|
+    if request.env["REQUEST_PATH"].include? ".pdf"
+      "file://#{Rails.root.join('app')}"
+    else
+      "#{request.protocol}#{request.host_with_port}"
+    end
+  }
+end
