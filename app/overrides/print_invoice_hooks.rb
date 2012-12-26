a55f801cb33b36b74261144a763f83d562c3de97
Stephane Bounmy <stephanebounmy@gmail.com>
2012-12-26 22:23:54 +0100

    fixed override + icon

--- app/overrides/print_invoice_hooks.rb
+++ app/overrides/print_invoice_hooks.rb
@@ -1,11 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                      :name => "admin_order_show_print_buttons",
-                     :insert_before => "[data-hook='admin_order_show_buttons']",
+                     :insert_after => 'code[erb-silent]:contains("content_for :page_actions do")',
                      :partial => "spree/admin/orders/print_buttons",
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
                      :name => "admin_order_edit_print_buttons",
-                     :insert_before => "[data-hook='admin_order_edit_buttons']",
+                     :insert_after => 'code[erb-silent]:contains("content_for :page_actions do")',
                      :partial => "spree/admin/orders/print_buttons",
                      :disabled => false)
