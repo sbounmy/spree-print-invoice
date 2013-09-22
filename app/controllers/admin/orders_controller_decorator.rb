a27c0d6fd00c06d205273b7ff45858fa4fb6920c
Stephane Bounmy <stephanebounmy@gmail.com>
2013-09-22 16:58:55 +0200

    added admin/spree-print-invoice controller

---
+++ app/controllers/admin/orders_controller_decorator.rb
@@ -0,0 +1,6 @@
+  Spree::Admin::OrdersController.class_eval do
+    def print
+      @hide_prices = params[:template] == "packaging_slip"
+      render :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'pdfkit'
+    end
+  end
--- lib/spree_print_invoice/engine.rb
+++ lib/spree_print_invoice/engine.rb
@@ -2,14 +2,6 @@ module PrintInvoice
   class Engine < Rails::Engine
 
     def self.activate
-
-      Spree::Admin::OrdersController.class_eval do
-        def print
-          @hide_prices = params[:template] == "packaging_slip"
-          render :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'pdfkit'
-        end
-      end
-
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
