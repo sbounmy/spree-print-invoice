f9a531cb3a02c7ea22b322d4e8b6f914ae83b5f2
Remy Bounthong <remy.bounthong@gmail.com mailto:remy.bounthong@gmail.com>
2013-09-26 16:12:11 +0200

    add frontend invoice orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Spree::Core::Engine.routes.draw do
   match "/admin/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :admin_print_order
+
+  #add invoice print button for clients /account
+  match "/orders/:id/print", :to => 'admin/orders#print', :via => :get, :as => :client_print_order
 end
