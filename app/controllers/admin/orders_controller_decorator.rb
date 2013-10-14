bdff8b4482ebe7209c0a9851c294008345adec63
Stephane Bounmy <stephanebounmy@gmail.com>
2013-10-14 22:06:25 +0200

    loading order

--- app/controllers/admin/orders_controller_decorator.rb
+++ app/controllers/admin/orders_controller_decorator.rb
@@ -1,5 +1,6 @@
   Spree::Admin::OrdersController.class_eval do
     def print
+      load_order
       @hide_prices = params[:template] == "packaging_slip"
       render :template => "spree/admin/orders/#{params[:template] || "invoice"}.html.erb", :layout => 'pdfkit'
     end
