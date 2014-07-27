3720cd73de8fa5446a41742403063496659848ce
Stephane Bounmy <stephanebounmy@gmail.com>
2014-07-27 21:12:29 +0200

    overrides compatible new deface

--- app/overrides/print_invoice_hooks.rb
+++ app/overrides/print_invoice_hooks.rb
@@ -1,12 +1,12 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                      :name => "admin_order_show_print_buttons",
-                     :insert_after => 'code[erb-silent]:contains("content_for :page_actions do")',
+                     :insert_after => 'erb[silent]:contains("content_for :page_actions do")',
                      :partial => "spree/admin/orders/print_buttons",
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
                      :name => "admin_order_edit_print_buttons",
-                     :insert_after => 'code[erb-silent]:contains("content_for :page_actions do")',
+                     :insert_after => 'erb[silent]:contains("content_for :page_actions do")',
                      :partial => "spree/admin/orders/print_buttons",
                      :disabled => false)
 
